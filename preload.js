dff2f894f1216dc338490003c6b2c03e69d06b41
sporollan <santiago.porollan@gmail.com>
2025-08-29 15:14:59 -0300

    enable preview

--- preload.js
+++ preload.js
@@ -3,5 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   send: (channel, data) => ipcRenderer.send(channel, data),
   receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-  selectCsvFile: () => ipcRenderer.invoke("select-csv-file")  // 👈 custom bridge
+  selectCsvFile: () => ipcRenderer.invoke("select-csv-file")
 });
